e2f71a5a88e6bc018824f0853f173ca653c09e86
Tomek Osiński <osinstom@gmail.com>
2017-08-22 17:55:46 +0200

    Changes for installing XMPP Provider bundle

--- providers/xmpp/pubsub/src/main/java/org/onosproject/provider/xmpp/pubsub/impl/XmppPubSubProvider.java
+++ providers/xmpp/pubsub/src/main/java/org/onosproject/provider/xmpp/pubsub/impl/XmppPubSubProvider.java
@@ -5,6 +5,10 @@ import org.onosproject.core.CoreService;
 import org.onosproject.net.provider.AbstractProvider;
 import org.onosproject.net.provider.ProviderId;
 import org.onosproject.xmpp.XmppController;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * XMPP PubSub extension provider.
@@ -12,11 +16,10 @@ import org.onosproject.xmpp.XmppController;
 @Component(immediate = true)
 public class XmppPubSubProvider extends AbstractProvider {
 
+    private final Logger logger = getLogger(getClass());
+
     protected static final String XMPP = "xmpp";
     private static final String PROVIDER = "org.onosproject.provider.xmpp.pubsub";
-
-    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
-    protected CoreService coreService;
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected XmppController controller;
@@ -26,12 +29,12 @@ public class XmppPubSubProvider extends AbstractProvider {
     }
 
     @Activate
-    public void activate() {
-
+    public void activate(ComponentContext context) {
+        logger.info("Started");
     }
 
     @Deactivate
-    public void deactivate() {
+    public void deactivate(ComponentContext context) {
 
     }
 
